931f6694d9a23039cc40497eb3422bdc6bd9cbeb
Tazar Chriss <tazar@example.com>
2026-09-09 07:21:04 +0300

    fic

--- routes/console.php
+++ routes/console.php
@@ -18,15 +18,15 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new ExpireBookingHoldsJob)->everyFiveMinutes();
-Schedule::job(new SendPreArrivalMessageJob)->dailyAt('09:00');
-Schedule::job(new SendCheckInNotificationJob)->dailyAt('08:00');
-Schedule::job(new SendCheckoutNotificationJob)->dailyAt('08:30');
-Schedule::job(new GenerateRevenueSnapshotJob)->dailyAt('01:00');
+Schedule::job(ExpireBookingHoldsJob::class)->everyFiveMinutes();
+Schedule::job(SendPreArrivalMessageJob::class)->dailyAt('09:00');
+Schedule::job(SendCheckInNotificationJob::class)->dailyAt('08:00');
+Schedule::job(SendCheckoutNotificationJob::class)->dailyAt('08:30');
+Schedule::job(GenerateRevenueSnapshotJob::class)->dailyAt('01:00');
 
 if (Setting::getValue('pricing_auto_generate_enabled', false)) {
     $frequency = Setting::getValue('pricing_auto_generate_frequency', 'weekly');
-    $schedule = Schedule::job(new GenerateSeasonalPricingJob);
+    $schedule = Schedule::job(GenerateSeasonalPricingJob::class);
     match ($frequency) {
         'daily' => $schedule->dailyAt('02:00'),
         'weekly' => $schedule->weeklyOn(Schedule::MONDAY, '02:00'),
@@ -37,7 +37,7 @@ if (Setting::getValue('pricing_auto_generate_enabled', false)) {
 
 if (Setting::getValue('schedule_beds24_sync_bookings_enabled', true)) {
     $frequency = Setting::getValue('schedule_beds24_sync_bookings_frequency', 'every_five_minutes');
-    $schedule = Schedule::job(new SyncBeds24BookingsJob);
+    $schedule = Schedule::job(SyncBeds24BookingsJob::class);
     match ($frequency) {
         'every_five_minutes' => $schedule->everyFiveMinutes(),
         'every_fifteen_minutes' => $schedule->everyFifteenMinutes(),
@@ -51,7 +51,7 @@ if (Setting::getValue('schedule_beds24_sync_bookings_enabled', true)) {
 
 if (Setting::getValue('schedule_beds24_sync_messages_enabled', true)) {
     $frequency = Setting::getValue('schedule_beds24_sync_messages_frequency', 'every_five_minutes');
-    $schedule = Schedule::job(new SyncBeds24MessagesJob);
+    $schedule = Schedule::job(SyncBeds24MessagesJob::class);
     match ($frequency) {
         'every_five_minutes' => $schedule->everyFiveMinutes(),
         'every_fifteen_minutes' => $schedule->everyFifteenMinutes(),
@@ -65,7 +65,7 @@ if (Setting::getValue('schedule_beds24_sync_messages_enabled', true)) {
 
 if (Setting::getValue('schedule_beds24_push_rates_enabled', true)) {
     $frequency = Setting::getValue('schedule_beds24_push_rates_frequency', 'hourly');
-    $schedule = Schedule::job(new PushBeds24RatesJob);
+    $schedule = Schedule::job(PushBeds24RatesJob::class);
     match ($frequency) {
         'every_five_minutes' => $schedule->everyFiveMinutes(),
         'every_fifteen_minutes' => $schedule->everyFifteenMinutes(),
